8cb6eb68fe2701f27cd1b585442cd9c1ac4fbd53
Julian7668 <julianeslava7668@gmail.com>
2025-09-14 10:50:56 -0500

    Delete modules/python_modules directory

--- modules/python_modules/utils/MDflip_coin.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-def flip_coin() -> str:
-    return random.choice(["Cara", "Cruz"])
--- modules/python_modules/utils/MDsearch_key_in_dict.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional, TypeVar
-
-K = TypeVar("K")
-V = TypeVar("V")
-
-
-def search_key_in_dict(diccionario: dict[K, V], valor: V) -> Optional[K]:
-    return next((k for k, v in diccionario.items() if v == valor), None)
