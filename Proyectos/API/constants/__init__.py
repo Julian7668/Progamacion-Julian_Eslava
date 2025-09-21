6594548b3543c99d4b7c44df0991b64cd752978a
Julian7668 <julianeslava7668@gmail.com>
2025-09-21 18:56:36 -0500

    Delete Proyectos/API directory

--- Proyectos/API/constants/__init__.py
+++
@@ -1,4 +0,0 @@
-from .constants import DIR_DATA, DATA_JSON, DELETED_JSON
-from .modelos import Tarea, TareaUpdate
-
-__all__ = ["DIR_DATA", "DATA_JSON", "DELETED_JSON", "Tarea", "TareaUpdate"]
--- Proyectos/API/constants/constants.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Archivos JSON donde guardaremos los datos
-DIR_DATA = "data"
-DATA_JSON = os.path.join(DIR_DATA, "tareas.json")
-DELETED_JSON = os.path.join(DIR_DATA, "tareas_eliminadas.json")
